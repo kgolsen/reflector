a3a0827998e95d22c45b4fabd06ded05399cd045
Kyle Olsen <kyle.g.olsen@gmail.com>
2020-04-13 15:58:54 -0700

    Still dinking around

--- reflector/cli.py
+++ reflector/cli.py
@@ -9,27 +9,39 @@ import click
 
 
 @click.group()
+@click.option('--sql-driver', help='SQL driver to use for connections (currently only psycopg2)')
+@click.option('-a', '--host', required=True, help='DB hostname')
+@click.option('-p', '--port', required=True, type=int, help='DB listener port')
+@click.option('-u', '--user', required=True, help='username to connect as')
+@click.option('-P', '--password', required=True, prompt=True, hide_input=True,
+              confirmation_prompt=True, help='DB user password')
+@click.option('-d', '--database', required=True, help='DB to reflect')
 def reflect():
     pass
 
 
 @reflect.command()
-def config():
-    pass
-
-
-@reflect.command()
 def inspect(**kwargs):
+    """run reflection on target schema"""
     pass
 
 
 @reflect.command()
+@click.option('-o', '--output', type=str, help='file to write spec to')
 def emit(**kwargs):
+    """build OpenAPI spec"""
     pass
 
 
 @reflect.command()
 def implement(**kwargs):
+    """build REST API"""
+    pass
+
+
+@reflect.command()
+def run(**kwards):
+    """inspect, emit, implement"""
     pass
 
 
