7b52e8ea1c9975053acbb809fcdd96e334f3fa7c
Kyle Olsen <kyle.g.olsen@gmail.com>
2020-04-14 15:12:36 -0700

    Reflective

--- reflector/db.py
+++ reflector/db.py
@@ -4,10 +4,10 @@ from urllib.parse import quote
 from sqlalchemy import *
 
 
-class Inspector:
+class Reflector:
 
     def __init__(self, config):
-        assert {'host', 'port', 'user', 'password', 'database', 'sql-driver'} < config.keys()
+        assert {'host', 'port', 'user', 'password', 'database', 'sql-driver'} <= config.keys()
         self.host = config['host']
         self.port = config['port']
         self.user = config['user']
@@ -18,6 +18,9 @@ class Inspector:
                                  f"{self.user}:{self.password}" \
                                  f"@{self.host}:{self.port}/" \
                                  f"{self.database}"
+        self.tables = {}
+        self.views = {}
+        self._init_sql()
 
     def __getstate__(self):
         state = self.__dict__.copy()
@@ -30,6 +33,7 @@ class Inspector:
 
     def _init_sql(self):
         self.sql = create_engine(self.connection_string)
+        self.meta = inspect(self.sql)
 
     def persist(self, file):
         with open(file, 'wb') as f:
@@ -40,3 +44,12 @@ class Inspector:
         with open(file, 'rb') as f:
             obj = pickle.load(f)
             assert isinstance(obj, cls)
+            return obj
+
+    def reflect(self):
+        for tbl in self.meta.get_table_names():
+            self.tables[tbl] = {
+                'columns': self.meta.get_columns(tbl),
+                'primary_key': self.meta.get_pk_constraint(tbl),
+                'foreign_keys': self.meta.get_foreign_keys(tbl),
+            }
